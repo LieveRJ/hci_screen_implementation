50c8ee65e3b4b64ecbac0d58aca9953523c03858
LieveRJ <lievejilesen@live.nl>
2021-05-17 13:39:45 +0200

    updated camera screen

--- lib/camera_screen.dart
+++ lib/camera_screen.dart
@@ -10,83 +10,31 @@ class Camera extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Colors.red,
         ),
-        body: ListView(
-          children: <Widget>[
-            Padding(
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 15.0, vertical: 10),
-                child: new Container(
-                  child: new Column(
-                    children: <Widget>[
-                      Row(
-                        mainAxisSize: MainAxisSize.max,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                        ],
-                      ),
-                      new SizedBox(
-                        height: 10.0,
-                      ),
-                      Row(
-                          children: <Widget>[
-                            new Expanded(
-                                child: Padding(
-                                  padding: const EdgeInsets.only(right: 5.0),
-                                  child: new Container(
-                                    height: 150.0,
-                                    decoration: new BoxDecoration(
-                                      borderRadius: new BorderRadius.circular(5.0),
-                                      color: Colors.black.withOpacity(0.0),),
-                                    child: new Column(
-                                      mainAxisAlignment: MainAxisAlignment.center,
-                                      children: <Widget>[
-                                        new Icon(
-                                          Icons.drive_eta,
-                                          color: Colors.white,
-                                        ),
-                                        new Text("Place your returned item in a return box like the one shown to the right",
-                                            style: new TextStyle(color: Colors.black, fontSize: 30))
-                                      ],
-                                    ),
-                                  ),
-                                )),
-                            new Expanded(
-                                child: new Container(
-                                  height: 400.0,
-                                  child: Column(
-                                    children: <Widget>[
-                                      Expanded(
-                                        child: Padding(
-                                          padding:
-                                          const EdgeInsets.only(bottom: 2.5, right: 2.5),
-                                          child: new Container(
-                                            decoration: new BoxDecoration(
-                                                color: Colors.black.withOpacity(0.0),
-                                                borderRadius: new BorderRadius.circular(5.0)),
-                                            child: new Row(
-                                              mainAxisAlignment: MainAxisAlignment.center,
-                                              children: <Widget>[
-                                                Padding(
-                                                  padding: const EdgeInsets.only(right: 8.0),
-                                                  child: new Image.asset(
-                                                      'assets/return.png'
-                                                  ),
-                                                ),
-                                              ],
-                                            ),
-                                          ),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                )),
-                          ]),
-                    ],
+        body: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                SizedBox(height: 20),
+                Expanded(
+                  child: Image.asset('assets/barcode_scanner.jpg',
+                  width: 200,
+                  height: 200,
                   ),
-                )
-            ),
-          ],
-        )
+                  ),
+                Container(
+                alignment: Alignment.center,
+                height: 200,
+                  child: Text('Scan your barcode by using the camera',
+                  style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.0,
+                  color: Colors.red,
+          ),
+          ),
+        ),
+        ],
+        ),
     );
   }
 }
